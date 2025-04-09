70d222263887104479b96e84a323903f93b532d6
adlynfakhreyz <adlynuniacc@gmail.com>
2025-04-09 11:28:55 +0700

    init project

---
+++ src/main/java/id/ac/ui/cs/advprog/eventsphere/EventSphereApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.eventsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSphereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventSphereApplication.class, args);
+    }
+
+}
