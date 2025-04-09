4388313067cb2e02fac8bbaf3c65c21df77cac8c
adlynfakhreyz <adlynuniacc@gmail.com>
2025-04-09 11:52:49 +0700

    update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,12 @@
+val seleniumJavaVersion = "4.14.1"
+val seleniumJupiterVersion = "5.0.1"
+val webdrivermanagerVersion = "5.6.3"
+val junitJupiterVersion = "5.9.1"
+
 plugins {
     java
-    id("org.springframework.boot") version "3.4.4"
+    jacoco
+    id("org.springframework.boot") version "3.4.2"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
@@ -26,12 +32,57 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
     compileOnly("org.projectlombok:lombok")
+
     developmentOnly("org.springframework.boot:spring-boot-devtools")
+
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.projectlombok:lombok")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
+    testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
+    testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.register<Test>(name = "unitTest") {
+    description = "Runs unit tests."
+    group = "verification"
+
+    filter {
+        excludeTestsMatching("*FunctionalTest")
+    }
+}
+
+tasks.register<Test>(name = "functionalTest") {
+    description = "Runs functional tests."
+    group = "verification"
+
+    filter {
+        includeTestsMatching("*FunctionalTest")
+    }
+}
+
+tasks.test {
+    filter {
+        excludeTestsMatching("*FunctionalTest")
+    }
+
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+
+    reports {
+        xml.required.set(true)  // Enable XML report for SonarCloud
+        html.required.set(true) // Optional: Enables HTML report for local debugging
+    }
 }
 
 tasks.withType<Test> {
---
+++ src/main/java/id/ac/ui/cs/advprog/eventsphere/auth/model/User.java
@@ -0,0 +1,4 @@
+package id.ac.ui.cs.advprog.eventsphere.auth;
+
+public class User {
+}
