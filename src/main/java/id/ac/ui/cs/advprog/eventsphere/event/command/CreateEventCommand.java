1d4755160a025ce47553155e04c95f440d9eee7f
Staphlerr <belvaghaniabhinaya2020@gmail.com>
2025-04-11 02:33:45 +0700

    [RED] Add EventCommand skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eventsphere/event/command/CreateEventCommand.java
@@ -0,0 +1,20 @@
+// src/main/java/id/ac/ui/cs/advprog/eventsphere/event/command/CreateEventCommand.java
+package id.ac.ui.cs.advprog.eventsphere.event.command;
+
+import id.ac.ui.cs.advprog.eventsphere.event.model.Event;
+import id.ac.ui.cs.advprog.eventsphere.event.service.EventService;
+
+public class CreateEventCommand implements EventCommand {
+    private final EventService eventService;
+    private final Event event;
+
+    public CreateEventCommand(EventService eventService, Event event) {
+        this.eventService = eventService;
+        this.event = event;
+    }
+
+    @Override
+    public void execute() {
+        // Placeholder implementation
+    }
+}
---
+++ src/main/java/id/ac/ui/cs/advprog/eventsphere/event/command/UpdateStatusCommand.java
@@ -0,0 +1,21 @@
+// src/main/java/id/ac/ui/cs/advprog/eventsphere/event/command/UpdateStatusCommand.java
+package id.ac.ui.cs.advprog.eventsphere.event.command;
+
+import id.ac.ui.cs.advprog.eventsphere.event.service.EventService;
+
+public class UpdateStatusCommand implements EventCommand {
+    private final EventService eventService;
+    private final String eventId;
+    private final String status;
+
+    public UpdateStatusCommand(EventService eventService, String eventId, String status) {
+        this.eventService = eventService;
+        this.eventId = eventId;
+        this.status = status;
+    }
+
+    @Override
+    public void execute() {
+        // Placeholder implementation
+    }
+}
