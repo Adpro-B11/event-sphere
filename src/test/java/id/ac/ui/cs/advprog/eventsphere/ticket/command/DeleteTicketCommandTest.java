7eb7d1f317679cea561735e8d637f586beaaad78
Ariqmau <ariqmalik@gmail.com>
2025-04-11 20:35:55 +0700

    [RED] Add tests for DeleteTicketCommand

---
+++ src/test/java/id/ac/ui/cs/advprog/eventsphere/ticket/command/DeleteTicketCommandTest.java
@@ -0,0 +1,27 @@
+package id.ac.ui.cs.advprog.eventsphere.ticket.command;
+
+import id.ac.ui.cs.advprog.eventsphere.ticket.repository.TicketRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+public class DeleteTicketCommandTest {
+
+    private TicketRepository repository;
+    private DeleteTicketCommand command;
+    private final String ticketId = "ticket123";
+
+    @BeforeEach
+    void setUp() {
+        repository = mock(TicketRepository.class);
+        command = new DeleteTicketCommand(repository, ticketId);
+    }
+
+    @Test
+    void testExecuteDeletesTicketFromRepository() {
+        command.execute();
+
+        verify(repository, times(1)).delete(ticketId);
+    }
+}
