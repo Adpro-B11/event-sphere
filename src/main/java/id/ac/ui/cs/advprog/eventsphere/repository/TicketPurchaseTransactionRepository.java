8eebbeade4f72ff8842ae6fee34564d9b0489a1a
Simforianus.Jonathan <simforianus.jonathan@ui.ac.id>
2025-04-11 19:37:43 +0700

    [GREEN] Implement TicketPurchaseRepository

--- src/main/java/id/ac/ui/cs/advprog/eventsphere/repository/TicketPurchaseTransactionRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eventsphere/repository/TicketPurchaseTransactionRepository.java
@@ -8,18 +8,28 @@ public class TicketPurchaseTransactionRepository {
     private final Map<String, TicketPurchaseTransaction> transactions = new HashMap<>();
 
     public TicketPurchaseTransaction save(TicketPurchaseTransaction trx) {
+        transactions.put(trx.getTransactionId(), trx);
+        return trx;
     }
 
     public TicketPurchaseTransaction findById(String id) {
+        return transactions.get(id);
     }
 
     public List<TicketPurchaseTransaction> findAll() {
+        return new ArrayList<>(transactions.values());
     }
 
     public List<TicketPurchaseTransaction> findByUserId(String userId) {
+        return transactions.values().stream()
+                .filter(trx -> trx.getUserId().equals(userId))
+                .toList();
     }
 
     public List<TicketPurchaseTransaction> findByStatus(String status) {
+        return transactions.values().stream()
+                .filter(trx -> trx.getStatus().equals(status))
+                .toList();
     }
 
 }
