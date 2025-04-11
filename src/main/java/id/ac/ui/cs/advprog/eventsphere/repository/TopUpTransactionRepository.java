b5be1cd93771b6cfcc61f40826266cf72306d71d
Simforianus.Jonathan <simforianus.jonathan@ui.ac.id>
2025-04-11 19:25:07 +0700

    [GREEN] Implement TopUpTransaction Repository

--- src/main/java/id/ac/ui/cs/advprog/eventsphere/repository/TopUpTransactionRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eventsphere/repository/TopUpTransactionRepository.java
@@ -9,14 +9,25 @@ public class TopUpTransactionRepository {
     private final Map<String, TopUpTransaction> topUpTransactionStorage = new HashMap<>();
 
     public TopUpTransaction save(TopUpTransaction trx) {
+        topUpTransactionStorage.put(trx.getTransactionId(), trx);
+        return trx;
     }
 
     public TopUpTransaction findById(String id) {
+        return topUpTransactionStorage.get(id);
     }
 
     public List<TopUpTransaction> findAll() {
+        return new ArrayList<>(topUpTransactionStorage.values());
     }
 
     public List<TopUpTransaction> findByStatus(String status) {
+        List<TopUpTransaction> filtered = new ArrayList<>();
+        for (TopUpTransaction trx : topUpTransactionStorage.values()) {
+            if (trx.getStatus().equalsIgnoreCase(status)) {
+                filtered.add(trx);
+            }
+        }
+        return filtered;
     }
 }
