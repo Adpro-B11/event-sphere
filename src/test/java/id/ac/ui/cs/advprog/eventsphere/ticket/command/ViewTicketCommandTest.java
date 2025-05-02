4b243340b7dab585d1dff89bac40b83f5a88d074
Ariqmau <ariqmalik@gmail.com>
2025-05-02 19:47:07 +0700

    [RED] Add ViewTicketCommand Test

---
+++ src/test/java/id/ac/ui/cs/advprog/eventsphere/ticket/command/ViewTicketCommandTest.java
@@ -0,0 +1,46 @@
+package id.ac.ui.cs.advprog.eventsphere.ticket.command;
+
+import id.ac.ui.cs.advprog.eventsphere.ticket.enums.TicketType;
+import id.ac.ui.cs.advprog.eventsphere.ticket.model.Ticket;
+import id.ac.ui.cs.advprog.eventsphere.ticket.repository.TicketRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class ViewTicketCommandTest {
+
+    @Mock
+    private TicketRepository repository;
+
+    private String ticketId;
+    private Ticket mockTicket;
+    private ViewTicketCommand command;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        ticketId = "ticket-123";
+        mockTicket = new Ticket();
+        mockTicket.setId(ticketId);
+        mockTicket.setType(TicketType.VIP);
+
+        when(repository.findById(ticketId)).thenReturn(mockTicket);
+
+        command = new ViewTicketCommand(repository, ticketId);
+    }
+
+    @Test
+    void testExecute() {
+        // Action
+        command.execute();
+
+        // Assert
+        verify(repository, times(1)).findById(ticketId);
+        assertEquals(mockTicket, command.getResult());
+    }
+}
