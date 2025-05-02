78d44f7959ca9cc5a52ecb7dd43ed8683c79fac4
Ariqmau <ariqmalik@gmail.com>
2025-05-02 20:22:32 +0700

    [RED] Add view ticket service interface

--- src/main/java/id/ac/ui/cs/advprog/eventsphere/ticket/service/TicketService.java
+++ src/main/java/id/ac/ui/cs/advprog/eventsphere/ticket/service/TicketService.java
@@ -4,7 +4,11 @@ import id.ac.ui.cs.advprog.eventsphere.auth.model.User;
 import id.ac.ui.cs.advprog.eventsphere.ticket.enums.TicketType;
 import id.ac.ui.cs.advprog.eventsphere.ticket.model.Ticket;
 
+import java.util.List;
+import java.util.Map;
+
 public interface TicketService {
     Ticket createTicket(User user, String eventId, TicketType type, double price, int quota);
     void deleteTicket(User user, String ticketId);
+    Ticket viewTicket(String ticketId);
 }
