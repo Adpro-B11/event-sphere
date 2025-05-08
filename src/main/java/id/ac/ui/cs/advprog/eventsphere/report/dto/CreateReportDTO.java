f9d2959b84a01ecc5479c45fe51a26f3cb8df9b1
tkgalon <z41d123@gmail.com>
2025-05-08 22:56:44 +0700

    [REFACTOR] Change decorator for CreateReportDTO

--- src/main/java/id/ac/ui/cs/advprog/eventsphere/report/dto/CreateReportDTO.java
+++ src/main/java/id/ac/ui/cs/advprog/eventsphere/report/dto/CreateReportDTO.java
@@ -1,10 +1,14 @@
 package id.ac.ui.cs.advprog.eventsphere.report.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class CreateReportDTO {
     private String title;
     private String description;
