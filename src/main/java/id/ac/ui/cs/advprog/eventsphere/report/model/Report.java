fc85b4f3bcfe7235c8ee1b6a9a6b31501a7d9dd8
tkgalon <z41d123@gmail.com>
2025-05-08 17:40:50 +0700

    [REFACTOR] Change format time on Report to yyyy-MM-dd

--- src/main/java/id/ac/ui/cs/advprog/eventsphere/report/model/Report.java
+++ src/main/java/id/ac/ui/cs/advprog/eventsphere/report/model/Report.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.eventsphere.report.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -43,6 +44,7 @@ public class Report {
     private String status;
 
     @Column(name = "created_at", nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDateTime createdAt;
 
     @Column(name = "created_by", nullable = false)
