3559322abf480ad2cca65d744a27a5dabb72e513
tkgalon <z41d123@gmail.com>
2025-05-08 22:55:29 +0700

    [REFACTOR] Delete Notification (Change design pattern)

--- src/main/java/id/ac/ui/cs/advprog/eventsphere/report/service/NotificationService.java
+++
@@ -1,8 +0,0 @@
-package id.ac.ui.cs.advprog.eventsphere.report.service;
-
-/**
- * Service for sending notifications to users.
- */
-public interface NotificationService {
-    void sendNotification(String userId, String message);
-}
