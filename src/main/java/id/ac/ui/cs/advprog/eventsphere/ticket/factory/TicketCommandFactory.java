4c95be4948a381451038ef817ac61f521708d5ca
Ariqmau <ariqmalik@gmail.com>
2025-05-09 20:21:17 +0700

    [REFACTOR] Add TicketCommandFactory

---
+++ src/main/java/id/ac/ui/cs/advprog/eventsphere/ticket/factory/TicketCommandFactory.java
@@ -0,0 +1,35 @@
+package id.ac.ui.cs.advprog.eventsphere.ticket.factory;
+
+import id.ac.ui.cs.advprog.eventsphere.ticket.model.Ticket;
+import id.ac.ui.cs.advprog.eventsphere.ticket.repository.TicketRepository;
+import id.ac.ui.cs.advprog.eventsphere.ticket.command.*;
+
+import java.util.Map;
+
+public class TicketCommandFactory {
+    private final TicketRepository repository;
+
+    public TicketCommandFactory(TicketRepository repository) {
+        this.repository = repository;
+    }
+
+    public TicketCommand createTicketCommand(Ticket ticket) {
+        return new CreateTicketCommand(repository, ticket);
+    }
+
+    public TicketCommand deleteTicketCommand(String ticketId) {
+        return new DeleteTicketCommand(repository, ticketId);
+    }
+
+    public ViewTicketCommand viewTicketCommand(String ticketId) {
+        return new ViewTicketCommand(repository, ticketId);
+    }
+
+    public ViewEventTicketsCommand viewEventTicketsCommand(String eventId) {
+        return new ViewEventTicketsCommand(repository, eventId);
+    }
+
+    public TicketCommand updateTicketCommand(String ticketId, Map<String, Object> updates) {
+        return new UpdateTicketCommand(repository, ticketId, updates);
+    }
+}
