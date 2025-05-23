7fde5dc6f5defd392ef47b3bbc8585e343dfee38
tkgalon <z41d123@gmail.com>
2025-05-23 20:44:11 +0700

    [GREEN] Implement api async for fetch on ReportController

--- src/main/java/id/ac/ui/cs/advprog/eventsphere/report/controller/ReportController.java
+++ src/main/java/id/ac/ui/cs/advprog/eventsphere/report/controller/ReportController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * REST Controller responsible for handling report-related API endpoints.
@@ -107,6 +108,37 @@ public class ReportController {
     public ResponseEntity<ReportDetailDTO> getReportById(@PathVariable("reportId") UUID reportId) {
         Report report = reportService.findReportById(reportId);
         return ResponseEntity.ok(reportMapper.toReportDetailDTO(report));
+    }
+
+    /**
+     * Retrieves all reports asynchronously.
+     *
+     * @return List of all reports
+     */
+    @GetMapping("/async")
+    public CompletableFuture<ResponseEntity<List<ReportListDTO>>> getAllReportsAsync() {
+        return reportService.findAllReportAsync()
+                .thenApply(reports -> ResponseEntity.ok(reportMapper.toReportListDTOs(reports)))
+                .exceptionally(ex -> {
+                    // Handle error jika terjadi exception
+                    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+                });
+    }
+
+    /**
+     * Retrieves all reports submitted by a specific user asynchronously.
+     *
+     * @param userId ID of the user whose reports to find
+     * @return List of reports associated with the specified user
+     */
+    @GetMapping("/user/{userId}/async")
+    public CompletableFuture<ResponseEntity<List<ReportListDTO>>> getReportByUserAsync(@PathVariable("userId") String userId) {
+        return reportService.findReportByUserAsync(userId)
+                .thenApply(reports -> ResponseEntity.ok(reportMapper.toReportListDTOs(reports)))
+                .exceptionally(ex -> {
+                    // Handle error jika terjadi exception
+                    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+                });
     }
 
 }
