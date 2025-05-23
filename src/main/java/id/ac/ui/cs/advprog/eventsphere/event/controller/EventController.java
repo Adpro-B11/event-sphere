082291ee8eb385c674eaddb6768b2de5f5cd761c
Staphlerr <belvaghaniabhinaya2020@gmail.com>
2025-05-23 16:13:38 +0700

    [GREEN] Refactor EventController for exposing async and api mapping

--- src/main/java/id/ac/ui/cs/advprog/eventsphere/event/controller/EventController.java
+++ src/main/java/id/ac/ui/cs/advprog/eventsphere/event/controller/EventController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 @RestController
-@RequestMapping("/events")
+@RequestMapping("api/events")
 public class EventController {
 
     private final EventService eventService;
