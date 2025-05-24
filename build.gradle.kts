33ad25347baa1cfebc95335a256607d4f77d021f
tkgalon <z41d123@gmail.com>
2025-05-24 16:17:12 +0700

    [CHORES] Add jwt security depedency on gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     // Dotenv
     implementation("io.github.cdimascio:dotenv-java:3.0.0")
-
+    implementation ("org.springframework.boot:spring-boot-starter-security")
+    implementation ("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly ("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly ("io.jsonwebtoken:jjwt-jackson:0.11.5")
     compileOnly("org.projectlombok:lombok")
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
