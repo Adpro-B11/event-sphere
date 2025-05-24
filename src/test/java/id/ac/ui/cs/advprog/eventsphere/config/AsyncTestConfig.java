fe7d9597c65a5bead7bf1a667224594eb9020dd0
adlynfakhreyz <adlynuniacc@gmail.com>
2025-05-24 10:49:28 +0700

    fix: add async config for test

---
+++ src/test/java/id/ac/ui/cs/advprog/eventsphere/config/AsyncTestConfig.java
@@ -0,0 +1,30 @@
+package id.ac.ui.cs.advprog.eventsphere.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@TestConfiguration
+@EnableAsync
+@Profile("test")
+public class AsyncTestConfig {
+    
+    @Bean(name = "taskExecutor")
+    @Primary
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(4);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("test-async-");
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setAwaitTerminationSeconds(60);
+        executor.initialize();
+        return executor;
+    }
+}
